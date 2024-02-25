cec34b89c1246c8133ca373d812368ba5aa8ceee
AKIN AKSOY <akinaksoyiletisim@gmail.com>
2024-02-25 22:46:50 +0300

    OLP-00002 REFACTOR BookController.java -> add getById method

--- src/main/java/org/open_library_project/be/book/controller/BookController.java
+++ src/main/java/org/open_library_project/be/book/controller/BookController.java
@@ -4,6 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.open_library_project.be.book.model.dto.book.request.BookCreateRequest;
 import org.open_library_project.be.book.model.entity.BookEntity;
 import org.open_library_project.be.book.service.BookCreateService;
+import org.open_library_project.be.book.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,12 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     private final BookCreateService bookCreateService;
+    private final BookService bookService;
 
     @PostMapping
     public BookEntity createBook(
             @RequestBody final BookCreateRequest bookCreateRequest
     ) {
         return bookCreateService.create(bookCreateRequest);
+    }
+
+    @GetMapping("/book/{bookId}")
+    public BookEntity getById(
+            @PathVariable("bookId") final String bookId
+    ) {
+        return bookService.getBookById(bookId);
     }
 
 }
