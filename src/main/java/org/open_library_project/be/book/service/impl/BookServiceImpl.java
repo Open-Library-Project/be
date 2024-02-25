022d9ba5cd8d8f3200d41bf56f1abe2f0d665cae
handenurduranozdemir <119211532+handenurduranozdemir@users.noreply.github.com>
2024-02-25 23:58:21 +0300

    OLP-00003 REFACTOR BookServiceImpl.java -> ADD getAllBooks method

--- src/main/java/org/open_library_project/be/book/service/impl/BookServiceImpl.java
+++ src/main/java/org/open_library_project/be/book/service/impl/BookServiceImpl.java
@@ -6,6 +6,8 @@ import org.open_library_project.be.book.repository.BookRepository;
 import org.open_library_project.be.book.service.BookService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 class BookServiceImpl implements BookService {
@@ -16,5 +18,10 @@ class BookServiceImpl implements BookService {
     ) {
         return bookRepository.findById(bookId)
                 .orElseThrow(() -> new RuntimeException("Bu id'ye sahip bir BookEntity bulunamadı!"));
+    }
+
+    @Override
+    public List<BookEntity> getAllBooks() {
+        return bookRepository.findAll();
     }
 }
